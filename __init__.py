e2f4162e194426cee8e956531da44a8c813c5a9c
Alexey Rubtsov <bagyrthelancer@gmail.com>
2011-09-07 13:05:41 +0400

    App skeleton.

---
+++ __init__.py
@@ -0,0 +1,20 @@
+from flask import Flask, jsonify, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def default():
+	"""Default route"""
+	return send_static_file('default.html')
+
+
+@app.route('/image/<url>', methods=['GET',])
+def procImg(url):
+	"""GET /image/<url>
+
+	Process image at url and returns stat."""
+    return jsonify(image=url)
+
+if __name__ == '__main__':
+    app.debug = False 
+    app.run(host='0.0.0.0')
