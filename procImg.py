fb646132263299599fab6d4f575557bdcb5df9ba
Alexey Rubtsov <bagyrthelancer@gmail.com>
2011-09-07 18:37:20 +0400

    Now it shold work

--- procImg.py
+++ procImg.py
@@ -5,10 +5,16 @@ def process(url):
     try:
         request = urllib2.urlopen(url)
     except URLError, e:
-        return {'result': 'error: ' + e.reason}
+        return {'result': 'error: ' + e.reason, 'size':(0,0), 'format': 'null'}
+
     try:
         outfile = './cache/' + url.split('/')[2].replace('.', '_') + '.img'
         outstream = file.open(outfile, 'wb')
         outstream.write(request.read)
     except IOError, e:
-        return {'result': 'error:' + e.reason}
+        return {'result': 'error:' + e.reason, 'size':(0,0), 'format': 'null'}
+
+    img = Image.open(outfile)
+    return {'result': 'success', 'size': img.size, 'format': img.format}
+
+    
