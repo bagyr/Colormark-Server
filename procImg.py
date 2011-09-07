cb73472bc2bda7917331aa51ec0a527c264bcf58
Alexey Rubtsov <bagyrthelancer@gmail.com>
2011-09-07 14:52:49 +0000

    Fixed ERRORS

--- procImg.py
+++ procImg.py
@@ -3,16 +3,17 @@ from PIL import Image
 
 def process(url):
     try:
-        request = urllib2.urlopen(url)
-    except URLError, e:
+        request = urllib2.urlopen('http://' + url + '/favicon.ico')
+    except urllib2.URLError, e:
         return {'result': 'error: ' + e.reason, 'size':(0,0), 'format': 'null'}
 
     try:
-        outfile = './cache/' + url.split('/')[2].replace('.', '_') + '.img'
-        outstream = file.open(outfile, 'wb')
-        outstream.write(request.read)
+        outfile = './cache/' + url.replace('.', '_') + '.img'
+        outstream = open(outfile, 'wb')
+        outstream.write(request.read())
+        outstream.close()
     except IOError, e:
-        return {'result': 'error:' + e.reason, 'size':(0,0), 'format': 'null'}
+        return {'result': 'error', 'size':(0,0), 'format': 'null'}
 
     img = Image.open(outfile)
     return {'result': 'success', 'size': img.size, 'format': img.format}
